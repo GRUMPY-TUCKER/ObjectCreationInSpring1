88fbb519149cae1cec461b78256ececb2a269f95
RUPAM SADHUKHAN <115567972+GRUMPY-TUCKER@users.noreply.github.com>
2025-02-16 15:08:36 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,19 @@
+package org.maven;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); //creates the container
+        Alien obj = (Alien) context.getBean("alien1");
+        System.out.println( "Hello World!" );
+        obj.code();
+    }
+}
